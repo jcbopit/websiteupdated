ddb9b488d39fd9d28b030cd869f2ec45101c88db
Justin Offutt <jcoffutt@iu.edu>
2025-12-26 00:15:36 -0500

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -26,18 +26,7 @@ const Contact = () => {
             </Button>
           </Link>
 
-          <Link href="https://www.instagram.com/suzoomie/reels/" isExternal style={{ width: '100%' }}>
-            <Button
-              leftIcon={<FaInstagram />}
-              width="100%"
-              bg="ghibli.moss"
-              color="ghibli.cream"
-              _hover={{ bg: 'ghibli.fern' }}
-              _active={{ bg: 'ghibli.ash' }}
-            >
-              Instagram
-            </Button>
-          </Link>
+        
 
           <Link href="https://www.linkedin.com/in/justinoffutt" isExternal style={{ width: '100%' }}>
             <Button
